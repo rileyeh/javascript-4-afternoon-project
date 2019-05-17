78c7171a33803481bd97ae7fa79228fbccb0eba5
rileyeh <rileyehatch@gmail.com>
2019-05-17 15:06:30 -0600

    classes done

--- classes.js
+++ classes.js
@@ -31,6 +31,20 @@
 
 //Code Here
 
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+  }
+
+  makeWidget(first_name, last_name) {
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+    
+}
+
 
 ////////// PROBLEM 2 //////////
 
@@ -48,6 +62,21 @@
 */
 
 //Code Here
+
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age, reports = []) {
+    super(first_name, last_name, email, age)
+    this.reports = reports
+    }
+
+  hire(employee) {
+    this.reports.push(employee)
+  }
+
+  fire(index) {
+    this.reports.splice(index, 1)
+  }
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -72,6 +101,39 @@
 */
 
 //Code Here
+
+class ProgressiveManager extends Manager {
+  constructor (first_name, last_name, email, age, reports, title = 'Not a manager', bonus = 0) {
+  super (first_name, last_name, email, age, reports)
+  this.title = title
+  this.bonus = bonus
+  }
+
+  hire(employee) {
+    this.reports.push(employee)
+    if (this.reports.length === 0) {
+      this.title = 'Not a manager'
+    } else if (this.reports.length >= 1 && this.reports.length <= 3) {
+      this.title = 'Barely Manager'
+    } else if (this.reports.length >= 4 && this.reports.length <= 10) {
+      this.title = 'Mostly Manager'
+    } else if (this.reports.length >= 11 && this.reports.length <= 50) {
+      this.title = 'Manager'
+    } else if (this.reports.length >= 51 && this.reports.length <= 100) {
+      this.title = 'Manager Plus'
+    } else if (this.reports.length >= 101) {
+      this.title = 'Bestest Manager'
+    }
+  }
+  
+
+  fire(index) {
+    this.reports.splice(index, 1)
+    this.bonus += 100
+  }
+
+
+}
 
 
 
@@ -99,5 +161,29 @@
 */
 
 //Code Here
+
+class Machine {
+  constructor (widgets_made_count = 0, wear_and_tear_count = 0, needs_reboot = false) {
+    this.widgets_made_count = widgets_made_count
+    this.wear_and_tear_count = wear_and_tear_count
+    this.needs_reboot = needs_reboot
+  }
+
+  makeWidgets(number) {
+    this.widgets_made_count += number
+    this.wear_and_tear_count = this.widgets_made_count / 50
+  }
+
+  fixMachine() {
+    this.needs_reboot = true
+  }
+
+  reboot() {
+    return() => {
+      this.wear_and_tear_count -= 10
+      this.needs_reboot = false
+    }
+  }
+}
 
 
